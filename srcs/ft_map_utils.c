659a8a157f1585e87535385768db558bd4e1b1be
jusato <jusato@student.42sp.org.br>
2022-10-02 08:23:36 +0200

    fixed memory leak in map reading, test is ok

--- srcs/ft_map_utils.c
+++ srcs/ft_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: jusato <jusato@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 02:36:52 by jusato            #+#    #+#             */
-/*   Updated: 2022/10/02 08:00:32 by jusato           ###   ########.fr       */
+/*   Updated: 2022/10/02 08:22:31 by jusato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ char	**ft_alloc_map_memory(t_map *map)
 char	**ft_scan_map(t_map *map, char *map_path)
 {
 	char	**map_mat;
+	char	*aux;
 	int		fd;
 	int		i;
 
@@ -33,13 +34,14 @@ char	**ft_scan_map(t_map *map, char *map_path)
 	if (!map_mat)
 		return (NULL);
 	fd = open(map_path, O_RDONLY);
-	ft_printf("file is opening\n");
 	i = 0;
 	while (1)
 	{
-		map_mat[i] = get_next_line(fd);
-		if (!map_mat[i])
+		aux = get_next_line(fd);
+		if (!aux)
 			break ;
+		map_mat[i] = aux;
+		free(aux);
 		i ++;
 	}
 	map_mat[i] = NULL;
@@ -56,9 +58,9 @@ void	ft_init_map(t_solong *game, int argc, char **argv)
 	if (ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4], ".ber", 4) != 0)
 		ft_exit(game, "Invalid map!\n", -1);
 	map_m = ft_scan_map(&game->map, argv[1]);
-	map_m = NULL;
-	// if (map_m == NULL)
-	// 	ft_exit(game, "failed to read map!", -1);
+	if (map_m == NULL)
+		ft_exit(game, "failed to read map!", -1);
 	game->map.map = map_m;
+	free(map_m);
 	return ;
 }
